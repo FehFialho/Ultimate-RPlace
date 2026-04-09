84975b4a4f48a4c2e6b55ca00e7c33ccc7093bfb
FehFialho <fernandaklechowicz@gmail.com>
2026-04-09 16:47:35 -0300

    Implement Login

--- front/src/app/domain/auth.api.ts
+++ front/src/app/domain/auth.api.ts
@@ -1,16 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Api } from "./api";
 import { LoginDto } from "./UserInterface";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class AuthApi extends Api{
-  login = (data: LoginDto) => {
-    return this.client.post(`${this.URL}/auth/login`, data).pipe();
+  login = (data: LoginDto) : Observable<string> => {
+    return this.client.post<string>(`${this.URL}/auth/login`, data).pipe();
   }
-  subscribe = (data: LoginDto) => {
-    return this.client.post(`${this.URL}/auth/subscribe`, data).pipe();
+  subscribe = (data: LoginDto) : Observable<void> =>  {
+    return this.client.post<void>(`${this.URL}/auth/subscribe`, data).pipe();
   }
 }
--- front/src/app/features/login-page/login-page.ts
+++ front/src/app/features/login-page/login-page.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { RegisterForm } from './register-form/register-form';
 import { LoginForm } from './login-form/login-form';
 import { CommonModule } from '@angular/common';
+import { AuthApi } from '../../domain/auth.api';
+import { LoginDto } from '../../domain/UserInterface';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   templateUrl: './login-page.html',
   styleUrls: ['./login-page.css'],
-  imports: [RouterOutlet, FormsModule, CommonModule, RegisterForm, LoginForm]
+  imports: [RouterOutlet, FormsModule, CommonModule, RegisterForm, LoginForm, ReactiveFormsModule]
 })
 export class LoginPage {
   isSubscribe = false;
   isRegister = true;
 
   constructor(
+    private api: AuthApi,
     private _router: Router,
     private _activatedRoute: ActivatedRoute
   ){}
@@ -25,5 +28,55 @@ export class LoginPage {
     this._activatedRoute.params.subscribe((params) => {
       console.log(params)
     })
+  }
+
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  subscribeForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  get Username(){
+    return this.loginForm.get("username")
+  }
+
+  get Password(){
+    return this.loginForm.get("password")
+  }
+
+  login = () => {
+    if (!this.loginForm.valid)
+    {
+      alert("Campos Inválidos")
+      return
+    }
+    const data: LoginDto = {
+      password: this.Password?.value,
+      username: this.Username?.value
+    }
+    this.api.login(data).subscribe(
+      res => {
+        this.isRegister = true
+        console.log (res)
+        sessionStorage.setItem("token", res);
+        location.reload();
+      });
+  }
+
+  subscribe = () => {
+    if(!this.loginForm.valid) {
+      alert("Campos Invalidos")
+      return;
+    }
+    const data: LoginDto = {
+      password: this.Password?.value,
+      username: this.Username?.value
+    }
+    this.api.subscribe(data).subscribe()
+    alert("Usuário criado com sucesso!")
   }
 }
